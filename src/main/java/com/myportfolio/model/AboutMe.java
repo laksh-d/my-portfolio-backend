f73c39e49c79d29f85347982eb28dc838a758b26
Laksh <lakshdevali05@gmail.com>
2025-07-19 18:17:27 +0530

    db update

--- src/main/java/com/myportfolio/model/AboutMe.java
+++ src/main/java/com/myportfolio/model/AboutMe.java
@@ -1,5 +1,6 @@
 package com.myportfolio.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
@@ -10,6 +11,8 @@ public class AboutMe {
 
     @Id
     private Integer id;
+
+    @Column(length = 512)
     private String introduction;
     private String address;
     private String phoneNo;
